f9b4eee6e1555ce8ae3e925402d2812b40fa77d2
Patrick Frantzen <patrick.frantzen@web.de>
2022-12-03 23:00:12 +0100

    implemented mobile device

--- classes/keyboard.classes.js
+++ classes/keyboard.classes.js
@@ -5,4 +5,74 @@ class Keyboard {
     DOWN = false;
     SPACE = false;
     D = false;
+
+    constructor() {
+        this.bindKeyPressEvents();
+        this.bindBtsPressEvents();
+    }
+
+    bindBtsPressEvents() {
+        if (this.mobile == true) {
+            document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
+                e.preventDefault();
+                this.LEFT = true;
+            });
+    
+            document.getElementById('btnLeft').addEventListener('touchend', (e) => {
+                e.preventDefault();
+                this.LEFT = false;
+            }); 
+        }
+    }
+
+    bindKeyPressEvents() {
+        window.addEventListener('keydown', (event) => {
+            if (event.keyCode == 39) {
+                keyboard.RIGHT = true;
+            }
+            if (event.keyCode == 37) {
+                keyboard.LEFT = true;
+            }
+            if (event.keyCode == 38) {
+                keyboard.UP = true;
+            }
+            if (event.keyCode == 40) {
+                keyboard.DOWN = true;
+            }
+            if (event.keyCode == 32) {
+                keyboard.SPACE = true;
+            }
+            if (event.keyCode == 68) {
+                keyboard.D = true;
+            }
+        });
+        
+        window.addEventListener('keyup', (event) => {
+            if (event.keyCode == 39) {
+                keyboard.RIGHT = false;
+                lastAction = new Date().getTime();
+            }
+            if (event.keyCode == 37) {
+                keyboard.LEFT = false;
+                lastAction = new Date().getTime();
+            }
+            if (event.keyCode == 38) {
+                keyboard.UP = false;
+                lastAction = new Date().getTime();
+            }
+            if (event.keyCode == 40) {
+                keyboard.DOWN = false;
+                lastAction = new Date().getTime();
+            }
+            if (event.keyCode == 32) {
+                keyboard.SPACE = false;
+                lastAction = new Date().getTime();
+            }
+            if (event.keyCode == 68) {
+                keyboard.D = false;
+                lastAction = new Date().getTime();
+            }
+        });
+    }
+
 }
--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ let world;
 let canvasheight = 480;
 let keyboard = new Keyboard();
 let lastAction = 0
+let mobile = false;
 
 
 function init() {
@@ -11,54 +12,17 @@ function init() {
     document.getElementById('startscreen').classList.add('d-none');
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
+    checkMobileDevice();
+    //stopGame();
 }
 
-
-
-window.addEventListener('keydown', (event) => {
-    if (event.keyCode == 39) {
-        keyboard.RIGHT = true;
+function checkMobileDevice() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // true for mobile device
+        document.getElementById('button-container').classList.remove('d-none');
+        mobile = true;
+    } else {
+        mobile = false;
     }
-    if (event.keyCode == 37) {
-        keyboard.LEFT = true;
-    }
-    if (event.keyCode == 38) {
-        keyboard.UP = true;
-    }
-    if (event.keyCode == 40) {
-        keyboard.DOWN = true;
-    }
-    if (event.keyCode == 32) {
-        keyboard.SPACE = true;
-    }
-    if (event.keyCode == 68) {
-        keyboard.D = true;
-    }
-});
-
-window.addEventListener('keyup', (event) => {
-    if (event.keyCode == 39) {
-        keyboard.RIGHT = false;
-        lastAction = new Date().getTime();
-    }
-    if (event.keyCode == 37) {
-        keyboard.LEFT = false;
-        lastAction = new Date().getTime();
-    }
-    if (event.keyCode == 38) {
-        keyboard.UP = false;
-        lastAction = new Date().getTime();
-    }
-    if (event.keyCode == 40) {
-        keyboard.DOWN = false;
-        lastAction = new Date().getTime();
-    }
-    if (event.keyCode == 32) {
-        keyboard.SPACE = false;
-        lastAction = new Date().getTime();
-    }
-    if (event.keyCode == 68) {
-        keyboard.D = false;
-        lastAction = new Date().getTime();
-    }
-});
+      
+}
