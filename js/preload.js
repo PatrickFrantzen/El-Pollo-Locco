49c2696f8aee840535112e12b93d037a81330f31
Patrick Frantzen <patrick.frantzen@web.de>
2023-01-12 21:06:44 +0100

    update

--- js/preload.js
+++ js/preload.js
@@ -51,11 +51,11 @@ function renderImages() {
     <img src="img/5_background/layers/1_first_layer/1.png"></img>
     <img src="img/5_background/layers/1_first_layer/2.png"></img>
 
-    <img src="img/5_background/layers/2_first_layer/1.png"></img>
-    <img src="img/5_background/layers/2_first_layer/2.png"></img>
+    <img src="img/5_background/layers/2_second_layer/1.png"></img>
+    <img src="img/5_background/layers/2_second_layer/2.png"></img>
 
-    <img src="img/5_background/layers/3_first_layer/1.png"></img>
-    <img src="img/5_background/layers/3_first_layer/2.png"></img>
+    <img src="img/5_background/layers/3_third_layer/1.png"></img>
+    <img src="img/5_background/layers/3_third_layer/2.png"></img>
 
     <img src="img/5_background/layers/4_clouds/1.png"></img>
     <img src="img/5_background/layers/4_clouds/2.png"></img>
